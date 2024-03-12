a08353cbbdfd38b4c404c4264e863cfcd170bd13
anatolipr <anatoli@impact.com>
2024-03-11 21:11:12 -0700

    improvements

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,25 +1,25 @@
 <script lang="ts" setup>
 
     import { DeepReadonly } from 'vue';
-import { addTodo, removeTodoItem, clearTodoItems,
+    import { addTodo, removeTodoItem, clearTodoItems,
     setTodoListNewValue, removeTodoList, setTodoItemCompleted,
     updateTodoItemName, updateTodoListName } from '../todo/todoStore';
 
-        import { TodoList } from '../todo/todoTypes';
+    import { TodoList } from '../todo/todoTypes';
 
-        const props = defineProps<{
-            idx: number,
-            todo: DeepReadonly<TodoList>
-        }>()
+    const props = defineProps<{
+        idx: number,
+        todo: DeepReadonly<TodoList>
+    }>()
 </script>
 
 <template
     ><div
-        style="position: relative; width: 354px; height: 537px; gap: 20px; padding: 12px; border: 1px solid gray; border-radius: 5px; background-color: #010101; display: flex; flex-direction: column">
-        <div
-            style="display: flex; align-items: center; justify-content: center">
+        style="position: relative; width: 354px; height: 537px"
+        class="list-tile">
+        <div class="list-heading">
             <div
-                style="font-size: 31px; flex: 1"
+                class="heading-text"
                 @click="() => updateTodoListName(props.idx)">
                 {{ todo.name }}
             </div>
@@ -28,25 +28,23 @@ import { addTodo, removeTodoItem, clearTodoItems,
                 class="trash-icon"
                 @click="removeTodoList(idx)"></div>
         </div>
-        <div
-            style="display: flex; align-items: center; justify-content: center">
+        <div class="list-stats">
             <div>{{ todo.stats.percent }}</div>
-            <div style="font-size: 16px; text-align: center; flex: 1">
+            <div class="todo-stats-main-text">
                 {{ todo.stats.unfinishedText }}
             </div>
             <div
-                style="width: 46px; height: 25px; border: 1px solid gray; border-radius: 5px; background-color: #580000; display: flex; align-items: center; justify-content: center"
-                @click="clearTodoItems(idx)">
+                style="width: 46px; height: 25px"
+                class="clear-btn"
+                @click="clearTodoItems(idx)"
+                v-if="todo.todoItems.length">
                 Clear
             </div>
         </div>
-        <div
-            style="overflow: scroll; padding: 4px; border: 1px solid gray; border-radius: 5px; flex: 1">
-            <div
-                style="gap: 5px; display: flex; align-items: center; justify-content: center"
-                v-for="(td, tdidx) in todo.todoItems">
+        <div style="overflow: scroll" class="todo-items-container">
+            <div class="todo-line" v-for="(td, tdidx) in todo.todoItems">
                 <input
-                    style="width: 33px; height: 34px; border: 1px solid gray; background-color: black!important"
+                    style="width: 33px; height: 34px"
                     type="checkbox"
                     @input="(e) => setTodoItemCompleted(idx, tdidx, (<HTMLInputElement>e.target).checked)"
                     :checked="td.completed" />
@@ -56,20 +54,21 @@ import { addTodo, removeTodoItem, clearTodoItems,
                 <div
                     style="width: 20px; height: 20px"
                     class="trash-icon"
-                    @click="() => removeTodoItem(idx, tdidx)"></div>
+                    @click="removeTodoItem(idx, tdidx)"></div>
             </div>
         </div>
-        <div style="gap: 10px; display: flex">
+        <div class="list-footer">
             <input
-                style="width: 208px; height: 47px; flex: 1"
+                style="height: 47px; flex: 1"
                 class="text-input"
                 type="text"
                 @input="(e) => setTodoListNewValue(idx, (<HTMLInputElement>e.target).value)"
                 :value="todo.newValue" />
             <button
-                style="width: 105px; height: 47px; border: 1px solid white; border-radius: 18px; background-color: #000000; display: flex; align-items: center; justify-content: center"
+                style="width: 105px; height: 47px"
+                class="std-button"
                 type="button"
-                @click="() => addTodo(props.idx)">
+                @click="addTodo(idx)">
                 Add
             </button>
         </div>
@@ -88,6 +87,76 @@ import { addTodo, removeTodoItem, clearTodoItems,
 
     .trash-icon {
       content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 21 21'%3E%3Cpath fill='none' stroke='white' stroke-linecap='round' stroke-linejoin='round' d='M5.5 4.5h10v12a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2zm5-2a2 2 0 0 1 1.995 1.85l.005.15h-4a2 2 0 0 1 2-2m-7 2h14m-9 3v8m4-8v8'/%3E%3C/svg%3E")
+    }
+
+    .todo-line {
+      gap: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center
+    }
+
+    .list-heading {
+      display: flex;
+      align-items: center;
+      justify-content: center
+    }
+
+    .list-stats {
+      display: flex;
+      align-items: center;
+      justify-content: center
+    }
+
+    .list-footer {
+      gap: 10px;
+      display: flex
+    }
+
+    .std-button {
+      border: 1px solid white;
+      border-radius: 18px;
+      background-color: #000000;
+      display: flex;
+      align-items: center;
+      justify-content: center
+    }
+
+    .list-tile {
+      gap: 20px;
+      padding: 12px;
+      border: 1px solid gray;
+      border-radius: 5px;
+      background-color: #010101;
+      display: flex;
+      flex-direction: column
+    }
+
+    .todo-items-container {
+      padding: 4px;
+      border: 1px solid gray;
+      border-radius: 5px;
+      flex: 1
+    }
+
+    .heading-text {
+      font-size: 31px;
+      flex: 1
+    }
+
+    .clear-btn {
+      border: 1px solid gray;
+      border-radius: 5px;
+      background-color: #580000;
+      display: flex;
+      align-items: center;
+      justify-content: center
+    }
+
+    .todo-stats-main-text {
+      font-size: 16px;
+      text-align: center;
+      flex: 1
     }
 
     * {box-sizing: border-box}
--- src/components/TodosContainer.vue
+++ src/components/TodosContainer.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
     import { DeepReadonly, Ref } from 'vue';
-import { $todo, addTodoList } from '../todo/todoStore';
-import { TodoMain } from '../todo/todoTypes';
+    import { $todo, addTodoList } from '../todo/todoStore';
+    import { TodoMain } from '../todo/todoTypes';
     import TodoList from './TodoList.vue';
     import {useStore} from '@nanostores/vue';
 
@@ -10,18 +10,36 @@ import { TodoMain } from '../todo/todoTypes';
 
 <template
     ><div
-        style="position: relative; width: 654px; overflow: scroll; gap: 20px; padding: 20px; border: 1px solid gray; display: flex; align-items: center; justify-content: flex-start">
+        style="position: relative; width: 654px; overflow: scroll"
+        class="horizontal-container">
         <todo-list
             v-for="(todoList, idx) in todo.todoLists"
             :todo="todoList"
             :idx="idx"
             :key="todoList.key" />
         <div
-            style="width: 69px; height: 68px; border-radius: 40px; border: 3px solid; content: url(&quot;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cpath fill='white' d='M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2'/%3E%3C/svg%3E&quot;); cursor: pointer"
+            style="width: 69px; height: 68px"
+            class="big-plus"
             @click="addTodoList('Unnamed', true)"></div></div
 ></template>
 
 <style>
+    .big-plus {
+      border-radius: 40px;
+      border: 3px solid;
+      content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cpath fill='white' d='M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2'/%3E%3C/svg%3E");
+      cursor: pointer
+    }
+
+    .horizontal-container {
+      gap: 20px;
+      padding: 20px;
+      border: 1px solid gray;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      resize: horizontal;
+    }
 
     * {box-sizing: border-box}
 </style>
