c97c6ecd9a08e74d56998a9a40fdd5f46b31aa1d
anatolipr <anatoli@impact.com>
2024-03-12 23:38:06 -0700

    vue dev tools

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { devtools } from '@nanostores/vue/devtools'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
+app.use(devtools,{})
